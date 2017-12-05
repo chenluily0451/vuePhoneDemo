b505059d1b3f1240cd94f5c6ae1b038c03c8a8e2
chenluily0451 <chenluily@163.com>
2017-12-05 10:21:35 +0800

    project init

---
+++ src/components/mainContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+      this is my main
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainContent'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
